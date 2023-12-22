f520685c6b413fdd928ab0941c987738a38fd098
malek bougadouha <bougadouha.malek@gmail.com>
2023-12-22 14:58:33 +0100

    init

--- mainRest.py
+++ mainRest.py
@@ -1,17 +1,14 @@
 import torch
 import numpy as np
-import argparse
 from PIL import Image
+from flask import Flask, request, jsonify
 from io import BytesIO
 import torch.nn.functional as F  # Import the functional module for additional functions
-
-
 
 import level1, ela
 from model import IMDModel
 
-
-
+app = Flask(__name__)
 
 def infer(img_path, model, device):
     print("Performing Level 1 analysis...")
@@ -34,24 +31,33 @@ def infer(img_path, model, device):
     print("Prediction:",end=' ')
     print("Authentic" if y_pred else "Tampared") # auth -> 1 and tp -> 0
     print("Probability:",predicted_probability)
+    result = {
+        'predicted_class': "Authentic" if y_pred else "Tampered"
+    }
+    return result
 
+@app.route('/detect_manipulation', methods=['POST'])
+def detect_manipulation():
+    try:
+        image = request.files['image'].read()
+        img = Image.open(BytesIO(image))
+        img_path = "temp/uploaded_image.jpg"
+        img.save(img_path,quality=100)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
 
+        model_path = "model/model_c1.pth"
+        model = torch.load(model_path)
+        result = infer(img_path,model,device)
+        return jsonify(result)
 
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='Image Manipulation Detection')
-
-    req_args = parser.add_argument_group('Required Args')
-    req_args.add_argument('-p', '--path', type=str, metavar='img_path', dest='img_path', required=True, help='Image Path')
-
-    args = parser.parse_args()
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #selecting device
-    print("Working on",device)
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Working on", device)
 
     model_path = "model/model_c1.pth"
     model = torch.load(model_path)
-
-    infer(model=model, img_path=args.img_path, device=device)
+    app.run(debug=True)
